561f65d5807f901ce78bbd3e8282a9590f2ba052
harirammoksha <hrchoudhary34@gmail.com>
2016-02-24 21:10:21 +0530

    #intial Commit
    Liyakat

---
+++ AngularJSAuthentication.ResourceServer/Controllers/ProtectedController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.ResourceServer.Controllers
+{
+    
+    [RoutePrefix("api/protected")]
+    public class ProtectedController : ApiController
+    {
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<object> Get()
+        {
+            //string token = "";
+            //Microsoft.Owin.Security.AuthenticationTicket ticket = Startup.OAuthBearerOptions.AccessTokenFormat.Unprotect(token);
+
+
+            var identity = User.Identity as ClaimsIdentity;
+
+            return identity.Claims.Select(c => new
+            {
+                Type = c.Type,
+                Value = c.Value
+
+            });
+
+        }
+        [Authorize]
+        [HttpGet]
+        public string Post(string str)
+        {
+            return str;
+        }
+    }
+
+
+}
